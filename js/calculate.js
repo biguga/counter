cedba4dc0c34a03ec7760f161dfd761a5ee4c109
biguga <adv06062001@gmail.com>
2023-05-29 20:48:33 +0500

    start2

---
+++ js/calculate.js
@@ -0,0 +1,8 @@
+/*const pristine = new Pristine(uploadForm, {
+    classTo: 'img-upload__field-wrapper',
+    errorTextParent: 'img-upload__field-wrapper',
+    errorTextClass: 'img-upload__field-wrapper__error-text'
+  });*/
+
+  const calculateButton = document.querySelector('form__submit-button');
+  calculateButton.disabled = false
---
+++ js/script.js
@@ -0,0 +1,1 @@
+import './calculate_button.js';
